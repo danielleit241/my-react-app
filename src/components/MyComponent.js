43d59936ab6d2eb9799c4de8c3c194533c2e87e5
danielleit241 <danielleee241@gmail.com>
2025-05-23 14:07:23 +0700

    Add UserInfo component: Implement user input handling for name and age

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,51 +1,12 @@
 import React from "react";
+import UserInfo from "./UserInfo";
 
 class MyComponent extends React.Component {
-  state = {
-    name: "Leit",
-    address: "123 Main St",
-    age: 21,
-  };
-
-  handleClick = (event) => {
-    this.setState({
-      name: "Daniel",
-      age: Math.floor(Math.random() * 100 + 1),
-    });
-    // this.setState({
-    //   age: Math.floor(Math.random() * 100 + 1),
-    // });
-  };
-
-  handleOnMouseOver = (event) => {
-    console.log(event);
-  };
-
-  handleOnChangeInput = (event) => {
-    console.log(event.target.value);
-    this.setState({
-      name: event.target.value,
-    });
-  };
-
-  handleOnSumit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-  };
-
   //JSX
   render() {
     return (
       <div>
-        My name is {this.state.name} and I'm {this.state.age} years old.
-        <form onSubmit={(event) => this.handleOnSumit(event)}>
-          <input
-            value={this.state.name}
-            type="text"
-            onChange={(event) => this.handleOnChangeInput(event)}
-          />
-          <button>Submit</button>
-        </form>
+        <UserInfo />
       </div>
     );
   }
---
+++ src/components/UserInfo.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+class UserInfo extends React.Component {
+  state = {
+    name: "Leit",
+    address: "123 Main St",
+    age: 21,
+  };
+
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleOnChangeAge = (event) => {
+    this.setState({
+      age: event.target.value,
+    });
+  };
+
+  handleOnSumit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} and I'm {this.state.age} years old.
+        <form onSubmit={(event) => this.handleOnSumit(event)}>
+          <lable>Your Name: </lable>
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChangeName(event)}
+          />
+          <br />
+          <lable>Your Age: </lable>
+          <input
+            value={this.state.age}
+            type="text"
+            onChange={(event) => this.handleOnChangeAge(event)}
+          />
+          <br />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UserInfo;
