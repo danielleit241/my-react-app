b3192b259ef50033527ecb29d8ac750a76a6b432
danielleit241 <danielleee241@gmail.com>
2025-05-23 13:38:14 +0700

    Add event handlers and buttons to MyComponent for interaction

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,21 @@ class MyComponent extends React.Component {
     age: 21,
   };
 
+  handleClick = (event) => {
+    console.log("My name is ", this.state.name);
+  };
+
+  handleOnMouseOver = (event) => {
+    console.log(event);
+  };
+
   //JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and I'm from {this.state.address}
+        <button onClick={this.handleClick}>Click me!</button>
+        <button onMouseOver={this.handleOnMouseOver}>Hover me!</button>
       </div>
     );
   }
