7a3a4f4f3e559bf6df6d31f728cd79b107b56037
danielleit241 <danielleee241@gmail.com>
2025-05-24 02:10:53 +0700

    Refactor App component to a functional component for improved simplicity

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import "./App.scss";
 import MyComponent from "./components/MyComponent";
 import React from "react";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <MyComponent />
-      </>
-    );
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <MyComponent />
+//       </>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <>
+      <MyComponent />
+    </>
+  );
+};
 
 export default App;
