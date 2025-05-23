e711df3f75a23fba8e9db866a2b585409367cbad
danielleit241 <danielleee241@gmail.com>
2025-05-23 13:14:53 +0700

    Refactor App component and add MyComponent

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world! This is a simple React app.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello world! This is a simple React app.
+      <MyComponent />
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>Hello world! This is a simple React app.</p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
