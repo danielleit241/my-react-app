355e6562a27ecbf57538bd51fd1faca49873a3bc
danielleit241 <danielleee241@gmail.com>
2025-05-24 00:08:44 +0700

    Add AddUserInfo component and integrate it into MyComponent; update DisplayInfo to correctly evaluate user age

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,21 +1,28 @@
 import React from "react";
-import UserInfo from "./UserInfo";
+import AddUserInfo from "./AddUserInfo";
 import DisplayInfo from "./DisplayInfo";
 
 class MyComponent extends React.Component {
   state = {
     listUsers: [
-      { id: 1, name: "Leit", age: 16 },
-      { id: 2, name: "Danile", age: 69 },
-      { id: 3, name: "Le", age: 23 },
+      { id: 1, name: "Leit", age: "16" },
+      { id: 2, name: "Danile", age: "69" },
+      { id: 3, name: "Le", age: "23" },
     ],
   };
+
+  handleAddNewUser = (user) => {
+    this.setState({
+      listUsers: [user, ...this.state.listUsers],
+    });
+  };
+
   //JSX
   //DRY: Don't Repeat Yourself
   render() {
     return (
       <div>
-        <UserInfo />
+        <AddUserInfo handleAddNewUser={this.handleAddNewUser} />
         <br />
         <br />
         <DisplayInfo listUsers={this.state.listUsers} />
