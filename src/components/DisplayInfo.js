cb861689760ab4c348c699cde06cbde2f6efaf17
danielleit241 <danielleee241@gmail.com>
2025-05-24 01:33:42 +0700

    Refactor DisplayInfo component to a functional component, removing state management and lifecycle methods for improved simplicity and performance

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -2,76 +2,62 @@ import React from "react";
 import "./DisplayInfo.scss";
 import logo from "../logo.svg";
 
-class DisplayInfo extends React.Component {
-  constructor(props) {
-    console.log(">>> call constructor");
-    super(props);
-    this.state = {
-      isShow: true,
-    };
-  }
-
-  componentDidMount() {
-    console.log(">>> componentDidMount");
-    setTimeout(() => {
-      document.title = "DisplayInfo";
-    }, 3000);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(">>> componentDidUpdate", this.props, prevProps);
-    if (this.props.listUsers !== prevProps.listUsers) {
-      if (this.props.listUsers.length === 5) {
-        alert("You have 5 users");
-      }
-    }
-  }
-
-  handleShowHide = (event) => {
-    this.setState({
-      isShow: !this.state.isShow,
-    });
-  };
-
-  render() {
-    console.log(">>> call render");
-    const { listUsers } = this.props;
-    //const listUsers = this.props.listUsers;
-    return (
-      <div className="display-info-container">
-        {/* <img src={logo} /> */}
-        <div>
-          <span
-            onClick={(event) => {
-              this.handleShowHide(event);
-            }}
-          >
-            {this.state.isShow ? "Hide" : "Show"} list users:
-          </span>
-        </div>
-        {this.state.isShow && (
-          <>
-            {listUsers.map((user) => {
-              return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div>My name is {user.name}</div>
-                  <div>My age is {user.age}</div>
-                  <div>
-                    <button
-                      onClick={() => this.props.handleDeleteUser(user.id)}
-                    >
-                      <span>Delete</span>
-                    </button>
-                  </div>
-                  <hr />
+//stateless vs. stateful
+// class DisplayInfo extends React.Component {
+//   render() {
+//     console.log(">>> call render");
+//     const { listUsers } = this.props;
+//     //const listUsers = this.props.listUsers;
+//     return (
+//       <div className="display-info-container">
+//         {true && (
+//           <>
+//             {listUsers.map((user) => {
+//               return (
+//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                   <div>My name is {user.name}</div>
+//                   <div>My age is {user.age}</div>
+//                   <div>
+//                     <button
+//                       onClick={() => this.props.handleDeleteUser(user.id)}
+//                     >
+//                       <span>Delete</span>
+//                     </button>
+//                   </div>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+const DisplayInfo = (props) => {
+  const { listUsers } = props;
+  return (
+    <div className="display-info-container">
+      {true && (
+        <>
+          {listUsers.map((user) => {
+            return (
+              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div>My name is {user.name}</div>
+                <div>My age is {user.age}</div>
+                <div>
+                  <button onClick={() => props.handleDeleteUser(user.id)}>
+                    <span>Delete</span>
+                  </button>
                 </div>
-              );
-            })}
-          </>
-        )}
-      </div>
-    );
-  }
-}
+                <hr />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfo;
