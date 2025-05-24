8a9b5b9b0fc8eb69a87db6f68ef0d7d15bcf3d4f
danielleit241 <danielleee241@gmail.com>
2025-05-25 03:49:01 +0700

    Update Header component to use NavLink for branding, add HomePage component, and adjust routing structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,9 @@ const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">Daniel Le</Navbar.Brand>
+        <NavLink className="navbar-brand" href="/">
+          Daniel Le
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="navbar-nav">
@@ -24,9 +26,9 @@ const Header = () => {
           </Nav>
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+              <NavDropdown.Item >Log in</NavDropdown.Item>
+              <NavDropdown.Item >Log out</NavDropdown.Item>
+              <NavDropdown.Item >Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = (props) => {
+  return (
+    <div className="homepage-container">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main landing page of our application.</p>
+      <p>Explore the features and functionalities we offer!</p>
+    </div>
+  );
+};
+
+export default HomePage;
