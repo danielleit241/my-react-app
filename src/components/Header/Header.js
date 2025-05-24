2ac53da1df1ee01746e8c3762d6ba55906607259
danielleit241 <danielleee241@gmail.com>
2025-05-25 03:41:49 +0700

    Update Header component to use NavLink for navigation and enhance styling in App.scss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -11,16 +11,16 @@ const Header = () => {
         <Navbar.Brand href="/">Daniel Le</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/" className="nav-link">
+          <Nav className="navbar-nav">
+            <NavLink to="/" className="nav-link">
               Home
-            </Link>
-            <Link to="/users" className="nav-link">
+            </NavLink>
+            <NavLink to="/users" className="nav-link">
               User
-            </Link>
-            <Link to="/admins" className="nav-link">
+            </NavLink>
+            <NavLink to="/admins" className="nav-link">
               Admin
-            </Link>
+            </NavLink>
           </Nav>
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
