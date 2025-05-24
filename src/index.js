df74d156fda3da53d9d686ba3ac998fa28c5c85a
danielleit241 <danielleee241@gmail.com>
2025-05-25 03:26:02 +0700

    Implement routing with React Router, add User and Admin components, and update Header for navigation

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from "./components/User/User";
+import Admin from "./components/Admin/Admin";
+import Header from "./components/Header/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="users" element={<User />} />
+      <Route path="admins" element={<Admin />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
