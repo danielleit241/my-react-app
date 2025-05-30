6c0e492efbb6b65781cf27491b31a31df15b9893
danielleit241 <danielleee241@gmail.com>
2025-05-31 01:37:06 +0700

    Refactor HomePage and Header components to enhance styling and layout; add login and signup buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,12 +24,14 @@ const Header = () => {
               Admin
             </NavLink>
           </Nav>
-          <Nav>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
+          <Nav className="ms-auto">
+            <button className="btn-login">Log in</button>
+            <button className="btn-signup">Sign up</button>
+            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item>Log in</NavDropdown.Item>
               <NavDropdown.Item>Log out</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -6,6 +6,16 @@ const HomePage = (props) => {
       <video autoPlay loop muted>
         <source src={videoHomepage} type="video/mp4" />
       </video>
+      <div className="homepage-content">
+        <div className="header">There's a better way to ask</div>
+        <div className="sub-header">
+          Collect all the data you need to understand customers with forms
+          designed to be refreshingly different.
+        </div>
+        <div className="button-container">
+          <button>Get Started. It's free</button>
+        </div>
+      </div>
     </div>
   );
 };
