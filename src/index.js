5f095ddaf0dc0d239081417812f2a0cdc19541f6
danielleit241 <danielleee241@gmail.com>
2025-06-20 18:47:32 +0700

    Add redux-persist for state persistence; update store configuration and index file for integration

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import Layout from "./Layout";
-import store from "./redux/store";
-import 'nprogress/nprogress.css';
+import { persistor, store } from "./redux/store";
+import "nprogress/nprogress.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 
